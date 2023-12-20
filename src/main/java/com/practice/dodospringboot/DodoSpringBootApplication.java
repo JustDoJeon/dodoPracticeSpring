c71f2e892dbe4b4e3e0015769c098d9749a81c67
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2023-12-20 23:06:30 +0900

    init commit

---
+++ src/main/java/com/practice/dodospringboot/DodoSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.practice.dodospringboot;
+
+import org.hibernate.dialect.sequence.HSQLSequenceSupport;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+DodoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoSpringBootApplication.class, args);
+	}
+
+}
